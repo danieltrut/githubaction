5b96ad6734324a4ab94cf0814221413387697d25
trutnev.daniil <trutnev.daniil@khk.ee>
2021-12-08 17:04:59 +0200

    Commit and push

--- .github/some.js
+++ .github/some.js
@@ -2,7 +2,7 @@ document.getElementById("demo").addEventListener("click", myFunction);
 
 function myFunction() {
     documentxsadcss.getElementById("demo").innerHTML = "YOU CLICKED ME!";
-} document.getElementById("demo").addEventListener("click", myFunction);
+} document.getElementById("demeso").addEventListener("click", myFunction);
 
 function myFunction() {
     document.getElementById("demo").innerHTML = "YOU CLICKED ME!";
